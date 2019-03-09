3de04dfefc5f690c3761270bc0ebad06955dbb15
visualstudio <visualstudio@122-PC37>
2019-03-08 19:59:37 -0500

    lab05

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -13,42 +13,28 @@ using System.Threading.Tasks;
 
 namespace Assignment2
 {
-    class Programa
+    public class Program
     {
-
         public static void Main()
         {
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
-            Console.ReadLine();
-
         }
+    }
 
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate();
 
-        public class DelegateExercises
+        void Method1()
         {
-            public delegate int MyDelegate(int intValue);
-
-            public int Method1(int intMethod1)
-            {
-                return intMethod1 * 2;
-            }
-
-            public int Method2(int intMethod2)
-            {
-                return intMethod2 * 10;
-            }
-
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                int result1 = myDelegate(10);
-                System.Console.WriteLine(result1);
-                myDelegate = new MyDelegate(Method2);
-                int result2 = myDelegate(10);
-                System.Console.WriteLine(result2);
-            }
+            System.Console.WriteLine("MyDelegate");
         }
 
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
     }
 }
